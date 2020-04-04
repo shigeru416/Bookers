486bca547660dcf81eae6bc4425ca38622c50de3
松崎匡浩 <matsuzakimasahiro@matsuzakimasashironoMacBook-puro.local>
2020-04-04 19:40:53 +0900

    投稿、編集、詳細画面の作成、noticeの表示設定の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,39 @@
 class BooksController < ApplicationController
   def top
   end
+  def index
+  end
+  def show
+    @book = Book.find(params[:id])
+
+  end
+  def new
+  	@book = Book.new
+  end
+  def create
+  	book = Book.new(book_params)
+    if book.save
+       redirect_to book_path(book.id), notice: 'Book was successfully created'
+    end
+  end
+  def edit
+    @book = Book.find(params[:id])
+  end
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+       redirect_to book_path(book.id), notice: 'Book was successfully updated'
+    end
+  end
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+       redirect_to books_path, notice: 'Book was successfully deleted'
+    end
+  end
+
+ private
+ def book_params
+  params.require(:book).permit(:title, :body)
+ end
 end
