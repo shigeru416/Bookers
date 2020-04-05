9dc5d5e250c92a8a5625beb58fb7ccdbfc9a8839
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-04-05 23:15:32 +0900

    Rspecテスト後修正（エラーメッセージ部分）

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,9 +22,11 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-       redirect_to book_path(book.id), notice: 'Book was successfully updated'
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book.id), notice: 'Book was successfully updated'
+    else
+       render :edit
     end
   end
   def destroy
