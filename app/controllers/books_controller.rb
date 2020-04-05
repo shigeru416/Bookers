18467caea3336e56faa7c2fbc4b7d364c54e5959
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-04-05 14:49:36 +0900

    新規投稿画面と一覧画面を同じページにまとめた

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,13 +3,11 @@ class BooksController < ApplicationController
   end
   def index
     @books = Book.all
+    @book = Book.new
   end
   def show
     @book = Book.find(params[:id])
 
-  end
-  def new
-  	@book = Book.new
   end
   def create
   	book = Book.new(book_params)
